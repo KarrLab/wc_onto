b8aedf8b85fc281b039578386a5205e8a06aca55
Jonathan Karr <jonrkarr@gmail.com>
2019-09-18 16:46:13 -0400

    changing default contact address

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     url="https://github.com/KarrLab/" + name,
     download_url='https://github.com/KarrLab/' + name,
     author="Karr Lab",
-    author_email="karr@mssm.com",
+    author_email="info@karrlab.org",
     license="MIT",
     keywords='whole-cell modeling single cell computational systems biology dynamical modeling bioinformatics ontology',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
